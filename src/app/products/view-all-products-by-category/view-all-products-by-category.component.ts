61e72cd9a1fcca9b478c58a1dc0fed1897e75b2b
pavani1710 <aindlapavani@gmail.com>
2024-05-27 10:16:08 +0530

    Crud Operation

---
+++ src/app/products/view-all-products-by-category/view-all-products-by-category.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-products-by-category',
+  templateUrl: './view-all-products-by-category.component.html',
+  styleUrl: './view-all-products-by-category.component.css'
+})
+export class ViewAllProductsByCategoryComponent {
+
+}
---
+++ src/app/products/view-all-products-by-date/view-all-products-by-date.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-products-by-date',
+  templateUrl: './view-all-products-by-date.component.html',
+  styleUrl: './view-all-products-by-date.component.css'
+})
+export class ViewAllProductsByDateComponent {
+
+}
