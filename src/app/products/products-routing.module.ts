d753d3b462b57aabb11d5def18523eccbf94d50e
pavani1710 <aindlapvani@gmail.com>
2024-05-27 18:12:27 +0530

    Update

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { ViewAllProductsComponent } from './view-all-products/view-all-products.component';
+import { ViewAllProductsByCategoryComponent } from './view-all-products-by-category/view-all-products-by-category.component';
+import { ViewAllProductsByDateComponent } from './view-all-products-by-date/view-all-products-by-date.component';
+import { DeleteProductComponent } from './delete-product/delete-product.component';
+import { UpdateProductComponent } from './update-product/update-product.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+const routes: Routes = [{ path: '', component: ProductsComponent },
+  { path : 'view-product/:id', component: ViewProductComponent},
+  { path : 'create-product', component: CreateProductComponent},
+  { path: 'list-products', component: ViewAllProductsComponent},
+  { path : 'search-category', component: ViewAllProductsByCategoryComponent },
+  { path : 'search-date', component: ViewAllProductsByDateComponent},
+  { path : 'delete-product', component: DeleteProductComponent},
+  { path : 'update-product/:id', component: UpdateProductComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/products/view-product/view-product.component.ts
+++ src/app/products/view-product/view-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-product',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class ViewProductComponent {
 
+  constructor(private activatedRoute: ActivatedRoute){
+
+  }
+
+  ngOnInit(): void{
+    
+  }
 }
